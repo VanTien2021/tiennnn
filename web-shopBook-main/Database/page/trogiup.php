3951a25a8c4d53630240838a08605a00a8c8fcdb
Van Tien <vantien07112002@gmail.com>
2021-12-17 13:02:11 +0700

    van tien nộp bài

---
+++ web-shopBook-main/Database/page/trogiup.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="vi">
+    <head>
+        <meta charset="utf-8">
+        <title>Tin Tức</title>
+        <link rel="stylesheet" href="../js/scrip.js">
+        <link rel="stylesheet" href="../css/Styles.css"> <!-- link tới file CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+    <div id = "main"><!--bao toàn bộ khung website-->
+            <div id ="head"><!--phần Banner-->
+                <h1>MUA SÁCH NHANH VÀ RẺ NHẤT</h1>
+                <h2>TẠI SHOP ONLINE</h2>
+               
+                <h3>HTT</h3>
+                <h4>(Những bộ sách sắp phát hành với giá ưu đãi cực sốc)</h4>
+                <div class="search-text">
+                    <input type="search" name="search" placeholder="Tìm kiếm">
+                    <a href="#"class="search-btn">
+                    <i class="fa fa-search"></i>
+                    </a>
+                </div>
+                <div class="logo">
+                  <img src="../img/logo.png" alt="logo"width="180px"; height="60px">
+                </div>
+                <div id = "mainmenu" >
+                  <ul>
+                    <a href="../view/dangky.php"><button>Đăng Ký</button></a>
+                    <a href="../login/index.php"><button>Đăng Nhập</button></a>
+                  </ul>
+                </div>
+            </div><!--End phần Banner-->
+            
+            <div id ="menutop"><!--phần Menutop-->
+                <ul>
+                    <li><a href="../php/index.php">Trang chủ</a></li>
+                    <li><a href="../page/gioithieu.php">Giới thiệu</a></li>
+                    <li><a href="#">Thể Loại</a>
+                      <ul >
+                        <li><a href="../view/kh.php">Khoa học </a></li>
+                        <li><a href="../view/tltc.php">Tâm lý-Tình cảm</a></li>
+                        <li><a href="../view/tthuyet.php">Tiểu thuyết </a></li>
+                        <li><a href="../view/vh.php">Văn học Việt Nam</a></li>
+                        <li><a href="../view/comic.php">Truyện tranh</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="../page/cachoatdong.php">Các hoạt động</a></li>
+                    <li><a href="../page/trogiup.php">Tin tức</a></li>
+                    <li><a href="../page/nhaxuatban.php">Nhà xuất bản</a></li> 
+                    <!--  icons giỏ hàng -->
+                    <a href="../view/giohang.php"> <svg  xmlns="http://www.w3.org/2000/svg" color="white" width="25" height="25" float="right" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16" >
+                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                    </svg>  </a>
+                </ul>
+            </div><!--End phần Menutop-->
+            <div class="container"> <!--Bìa quảng cáo-->
+                <div id ="maincontentgt"><!--phần bao của content-->                    
+                    <div class="album py-5 bg-light">
+                    <!-- them -->
+                    <div class="Box">
+<table border="0" width="100%" cellpadding="0" cellspacing="0">
+    <tbody><tr>
+        <td id="Left" width="712" valign="top" align="justify">
+           
+                <div class="Title" style="margin-top: 5px;">
+                    <div>
+                    <img src="../img/ttt.jpg" width="34px" height="34px">
+                        <a href="/tin_tuc/tabid/203/cateID/24/language/vi-VN/Default.aspx" '=""> Tin mới </a>
+                        
+                    </div>
+                    <span style="position:relative; top:0px; left:0px; width:auto; height:auto; font-size:11px; margin:7px 0px 0px 0px; color:#8b8b8b; float:right; padding-right:5px;">
+                        Thứ sáu, 17/12/2021 09:29
+                    </span>
+                </div>
+                
+                    <div class="NewsContent">
+                        <input type="hidden" name="dnn$ctr895$Default$wucTinTuc_Detail$hfArticleID" id="dnn_ctr895_Default_wucTinTuc_Detail_hfArticleID" value="21778">
+                        <div class="Name" style="padding-bottom:7px;">
+                            Lãnh đạo thành phố Hà Nội dâng hoa kỷ niệm 75 năm Ngày Toàn quốc kháng chiến
+                        </div>
+                        
+                        <div class="Name" style="color: #000;">
+                            <div style="font-size: 13px;"><p>Sáng 17-12, nhân kỷ niệm 75 năm  Ngày Toàn quốc kháng chiến (19/12/1946 - 19/12/2021), Đoàn đại biểu  Thành ủy - HĐND - UBND - Ủy ban Mặt trận Tổ quốc Việt Nam thành phố Hà  Nội đã đến dâng hoa tại Tượng đài “Quyết tử để Tổ quốc quyết sinh”, vườn  hoa Hàng Đậu, phường Quán Thánh, quận Ba Đình.</p>
+                            <p>&nbsp;</p></div>
+                        </div>
+                        
+                        <div class="Content">
+                            <p>&nbsp;</p>
+                            <p><strong><br>
+                            </strong></p>
+                            <div style="text-align:center"><figure class="image" style="display:inline-block">
+                            <div>
+                            <div class="thumbox"><img src="http://hanoimoi.com.vn/Uploads/images/tuandiep/2021/12/17/danghuong1.jpg" width="697" height="424" alt=""></div>
+                        </div>
+                        <figcaption>Các đồng chí lãnh đạo thành phố Hà Nội và đại diện Ban Liên  lạc truyền thống Cựu chiến binh Trung đoàn Thủ đô dâng hoa tại tượng đài  “Quyết tử để Tổ quốc quyết sinh”.</figcaption> </figure></div>
+                            <p style="text-align:justify">&nbsp;</p>
+                            <p style="text-align:justify">Tham gia đoàn có các đồng chí: Ủy viên Bộ  Chính trị, Bí thư Thành ủy, Trưởng đoàn đại biểu Quốc hội thành phố Đinh  Tiến Dũng; Ủy viên Trung ương Đảng, Phó Bí thư Thường trực Thành ủy  Nguyễn Thị Tuyến; Ủy viên Trung ương Đảng, Phó Bí thư Thành ủy, Chủ tịch  UBND thành phố Chu Ngọc Anh; Phó Bí thư Thành ủy, Chủ tịch HĐND thành  phố Nguyễn Ngọc Tuấn; Phó Bí thư Thành ủy Nguyễn Văn Phong; lãnh đạo một  số sở, ban, ngành, Bộ Tư lệnh Thủ đô và Ban Liên lạc truyền thống Cựu  chiến binh Trung đoàn Thủ đô.</p>
+                            <p style="text-align:justify">Cách đây 75 năm, trước dã tâm muốn cướp  nước ta một lần nữa của thực dân Pháp, ngày 19-12-1946, Chủ tịch Hồ Chí  Minh viết Lời kêu gọi Toàn quốc kháng chiến. Người hiệu triệu: “Hỡi đồng  bào toàn quốc! Chúng ta muốn hòa bình, chúng ta phải nhân nhượng. Nhưng  chúng ta càng nhân nhượng, thực dân Pháp càng lấn tới, vì chúng quyết  tâm cướp nước ta một lần nữa! Không! Chúng ta thà hy sinh tất cả, chứ  nhất định không chịu mất nước, nhất định không chịu làm nô lệ. Hỡi đồng  bào! Chúng ta phải đứng lên!”.</p>
+                            <p style="text-align:justify">&nbsp;</p>
+                        <div style="text-align:center"><figure class="image" style="display:inline-block">
+                            <div>
+                                <div class="thumbox"><img src="http://hanoimoi.com.vn/Uploads/images/tuandiep/2021/12/17/danghuongT1.jpg" width="693" height="436" alt=""></div>
+                            </div>
+                        <figcaption>Bí thư Thành ủy Hà Nội Đinh Tiến Dũng và các đồng chí lãnh  đạo thành phố Hà Nội dâng hoa kỷ niệm 75 năm Ngày Toàn quốc kháng chiến.</figcaption> </figure></div>
+                            <p style="text-align:justify">&nbsp;</p>
+                            <p style="text-align:justify">Thủ đô Hà Nội, nơi có Trung ương Đảng,  Chính phủ và Chủ tịch Hồ Chí Minh, là nơi thực dân Pháp có ý định đánh  úp cơ quan đầu não và tiêu diệt lực lượng vũ trang của ta đã trở thành  chiến trường chính trong những ngày đầu kháng chiến toàn quốc.</p>
+                            <p style="text-align:justify">Hưởng ứng Lời kêu gọi của Chủ tịch Hồ Chí  Minh, quân và dân Hà Nội đã giương cao ngọn cờ “Quyết tử để Tổ quốc  quyết sinh”, biến từng con phố, mỗi ngôi nhà trở thành chiến hào, chiến  lũy triển khai thế trận chiến tranh nhân dân, toàn dân đánh giặc.</p>
+                            <p style="text-align:justify">Trải qua 60 ngày đêm chiến đấu kiên cường,  anh dũng, sáng tạo, quân và dân Hà Nội hoàn thành xuất sắc nhiệm vụ bảo  vệ an toàn cho cơ quan đầu não, tản cư, bảo đảm an toàn cho nhân dân;  chuyển hàng nghìn tấn máy móc, vật tư ra an toàn khu, tạo tiềm lực ban  đầu cho kháng chiến. Trong cuộc chiến đấu anh dũng, kiên cường đó, quân  và dân Thủ đô đã đánh hàng trăm trận, làm tiêu hao nhiều sinh lực địch,  giam chân chúng dài ngày trong thành phố, tạo điều kiện để cả nước bước  vào chiến tranh, triển khai thế trận chiến đấu lâu dài.</p>
+                            <p style="text-align:justify">&nbsp;</p>
+                            <div style="text-align:center"><figure class="image" style="display:inline-block">
+                            <div>
+                                <div class="thumbox"><img src="http://hanoimoi.com.vn/Uploads/images/tuandiep/2021/12/17/danghuong5.jpg" width="699" height="354" alt=""></div>
+                            </div>
+                        <figcaption></figcaption></figure></div>
+                            <div style="text-align:center"><figure class="image" style="display:inline-block"><figcaption>Các đồng chí lãnh đạo thành phố Hà Nội và đại diện Ban Liên  lạc truyền thống Cựu chiến binh Trung đoàn Thủ đô dâng hoa tại Tượng đài  “Quyết tử để Tổ quốc quyết sinh”.</figcaption> </figure></div>
+                            <p style="text-align:justify">&nbsp;</p>
+                            <p style="text-align:justify">Tượng đài “Quyết tử để Tổ quốc quyết sinh”  được khánh thành năm 2004 là biểu tượng tôn vinh ý chí anh dũng&nbsp;kiên  cường&nbsp;hưởng ứng Lời kêu gọi Toàn quốc kháng chiến của Chủ tịch Hồ Chí  Minh, là sự ghi dấu chiến công anh hùng của quân và dân Thủ đô trong 60  ngày đêm cầm chân giặc Pháp cuối năm 1946, đầu năm 1947.</p>
+                            <p style="text-align:justify">Thành kính dâng hoa tại Tượng đài “Quyết  tử để Tổ quốc quyết sinh”, các đồng chí lãnh đạo thành phố bày tỏ lòng  biết ơn công lao đóng góp, sự hy sinh của các thế hệ cha anh đi trước,  những chiến sĩ, đồng bào đã làm nên 60 ngày đêm lịch sử, là niềm tự hào  của lớp lớp người Hà Nội hôm nay và mai sau.</p>
+                            <p style="text-align:justify">Các đồng chí lãnh đạo thành phố nguyện  quyết tâm lãnh đạo Đảng bộ, chính quyền, quân và dân Hà Nội tiếp tục  phát huy truyền thống đoàn kết, chung sức, chung lòng xây dựng và phát  triển Thủ đô xứng đáng với vai trò là trung tâm đầu não chính trị - hành  chính quốc gia, trung tâm lớn về văn hóa, khoa học, giáo dục, kinh tế  và giao dịch quốc tế, một động lực phát triển của vùng Đồng bằng sông  Hồng và cả nước; vừa làm tốt công tác phòng, chống dịch Covid-19 vừa  phục hồi, phát triển kinh tế, giữ vững an ninh - quốc phòng, bảo đảm an  sinh xã hội, ngày càng nâng cao đời sống vật chất, tinh thần cho nhân  dân.</p>
+                            <p>&nbsp;</p>
+                            <p>&nbsp;</p>
+                            <p style="text-align: right;"><em><strong>(Theo Văn Tin - Viết Thành/ Hà Nội moi.com.vn)</strong></em></p>
+                            <p>&nbsp;</p>
+                            <p>&nbsp;</p>
+                            <p>&nbsp;</p>
+                    </div>
+                </div>
+        </td>
+    </tr>
+    </tbody></table>
+</div>
+                    <!-- het -->
+                    </div>
+                </div>
+            </div>
+
+</main>
+    <div id = "footer"><!--phần cuối trang-->
+                CopyRight: Trang bán Sách<br>
+                Biên tập thiết kế, xây dựng và quản trị: Phạm Văn Tiến<br>
+                Chịu trách nhiệm chính: Đỗ Thị Đài Trang, Nguyễn Minh Hoàng
+                <div class="container">
+            <!-- icon mạng XH -->
+              <ul class="list-unstyled list-inline text-center">
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-fb mx-1">
+                    <i class="fab fa-facebook-f"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-tw mx-1">
+                    <i class="fab fa-twitter"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-gplus mx-1">
+                    <i class="fab fa-google"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-dribbble mx-1">
+                    <i class="fab fa-instagram"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-li mx-1">
+                    <i class="fab fa-github"> </i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-floating btn-dribbble mx-1">
+                    <i class="fab fa-youtube"> </i>
+                  </a>
+                </li>
+              </ul>
+              <!-- end icon mạng XH -->
+          </div>
+            </div><!--End phần cuối trang-->
+        </div><!--End bao toàn bộ khung websites-->
+    </body>
+</html>
